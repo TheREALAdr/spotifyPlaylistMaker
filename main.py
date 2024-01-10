c26471d01fc7210aad69a9d7f9b50c054ed000bd
TheREALAdr <anotidamurambadoro455@gmail.com>
2024-01-10 13:25:45 -0600

    Spotify OAuth request/redirect allows user to sign up with Spotify given a redirect uri and SFD info. Can currently pull profile info, including user ID, from users; playlist building with Billboard coming soon.

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ from spotipy.oauth2 import SpotifyOAuth
 CLIENTID = os.environ.get("CLIENTID")
 CLIENTSECRET = os.environ.get("CLIENTSECRET")
 REDIRECTURI = os.environ.get("REDIRECTURI")
+CLIENTUSERNAME = os.environ.get("CLIENTUSERNAME")
 
+scope = "playlist-modify-private"
 # WEBSITE_ACCESS_LINK = "https://www.billboard.com/charts/hot-100/"
 # movie_date = input("What time do you want to travel to? Type the date in YYYY-MM-DD format here: \n")
 #
@@ -21,7 +23,9 @@ REDIRECTURI = os.environ.get("REDIRECTURI")
 #     song_title = song_item.getText().strip()
 #     song_title_list.append(song_title)
 
-spotifyinit = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENTID, client_secret=CLIENTSECRET,
-                                                        redirect_uri=REDIRECTURI, scope="playlist-modify-private"))
+spotifyinit = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope, client_id=CLIENTID, client_secret=CLIENTSECRET,
+                                                        redirect_uri=REDIRECTURI, username=CLIENTUSERNAME))
 
-print(spotifyinit)
+current_user_info = spotifyinit.current_user()
+user_id = spotifyinit.current_user()["id"]
+
