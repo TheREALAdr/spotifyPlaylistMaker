ffbe54ed5365432a1fa8ffce393594ed423e04ce
TheREALAdr <anotidamurambadoro455@gmail.com>
2024-01-15 20:44:41 -0600

    Still working on direct track identification; so far, can pull songwriter information through SpotiPy. Project almost complete.

--- main.py
+++ main.py
@@ -10,22 +10,32 @@ REDIRECTURI = os.environ.get("REDIRECTURI")
 CLIENTUSERNAME = os.environ.get("CLIENTUSERNAME")
 
 scope = "playlist-modify-private"
-# WEBSITE_ACCESS_LINK = "https://www.billboard.com/charts/hot-100/"
-# movie_date = input("What time do you want to travel to? Type the date in YYYY-MM-DD format here: \n")
-#
-# song_title_list = []
-# # Pulling from Billboard website! Take data only when necessary!
-# response = requests.get(f"{WEBSITE_ACCESS_LINK}{movie_date}")
-# bs4_data = BeautifulSoup(response.content, "html.parser", from_encoding="utf-8")
-# song_data = bs4_data.select("li ul li h3")
-#
-# for song_item in song_data:
-#     song_title = song_item.getText().strip()
-#     song_title_list.append(song_title)
+WEBSITE_ACCESS_LINK = "https://www.billboard.com/charts/hot-100/"
+movie_date = input("What time do you want to travel to? Type the date in YYYY-MM-DD format here: \n")
 
-spotifyinit = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope, client_id=CLIENTID, client_secret=CLIENTSECRET,
+song_title_list = []
+# Pulling from Billboard website! Take data only when necessary!
+response = requests.get(f"{WEBSITE_ACCESS_LINK}{movie_date}")
+bs4_data = BeautifulSoup(response.content, "html.parser", from_encoding="utf-8")
+song_data = bs4_data.select("li ul li h3")
+
+for song_item in song_data:
+    song_title = song_item.getText().strip()
+    song_title_list.append(song_title)
+
+def find_track():
+    for song_item in song_data:
+        song_title = song_item.getText().strip()
+        song_title_list.append(song_title)
+        spotify_query = {
+            song_title
+        }
+        spotifyinit = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope, client_id=CLIENTID, client_secret=CLIENTSECRET,
                                                         redirect_uri=REDIRECTURI, username=CLIENTUSERNAME))
+        spotify_data = spotifyinit.search(q=spotify_query, limit=1, offset=0, type='track', market=None)
+        print(spotify_data)
 
-current_user_info = spotifyinit.current_user()
-user_id = spotifyinit.current_user()["id"]
+find_track()
+# current_user_info = spotifyinit.current_user()
+# user_id = spotifyinit.current_user()["id"]
 
